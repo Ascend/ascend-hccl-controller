4889958612e221c9c06b8def1c4b78da482591c1
BianTanggui <hudepeng2@huawei.com>
2020-11-11 10:58:08 +0800

    Match-id-83527a158a7a42eb01cf706e4910b8ac8a8c61cd

--- build/build.sh
+++ build/build.sh
@@ -7,14 +7,31 @@ CUR_DIR=$(dirname $(readlink -f $0))
 TOP_DIR=$(realpath ${CUR_DIR}/..)
 export GO111MODULE="on"
 unset GOPATH
-build_version="v20.1.0"
+VER_FILE="${TOP_DIR}"/service_config.ini
+build_version="beta"
+if  [ -f "$VER_FILE" ]; then
+  line=$(sed -n '3p' "$VER_FILE" 2>&1)
+  #cut the chars after ':'
+  build_version=${line#*:}
+fi
+
+arch=$(arch  2>&1)
+echo "Build Architecture is" "${arch}"
+if [ ${arch:0:5} = 'aarch' ];then
+  arch=arm64
+else
+  arch=amd64
+fi
+
+sed -i "s/hccl-controller:.*/hccl-controller:${build_version}/" "${TOP_DIR}"/build/hccl-controller.yaml
+
 build_time=$(date +'%Y-%m-%d_%T')
 OUTPUT_NAME="hccl-controller"
 
 
 
 DOCKER_FILE_NAME="Dockerfile"
-docker_zip_name="hccl-controller.tar.gz"
+docker_zip_name="hccl-controller-${build_version}-${arch}.tar.gz"
 docker_images_name="hccl-controller:${build_version}"
 function clear_env()
 {
