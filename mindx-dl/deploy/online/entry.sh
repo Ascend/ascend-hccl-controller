cb538eb54895a3cceee795277551705c129d3580
BianTanggui <kangfuan1@huawei.com>
2020-11-16 19:55:25 +0800

    Match-id-7dfe2ef0ff8f817872ee5a65f3d64de692781057

---
+++ mindx-dl/deploy/online/entry.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Entry script for online cluster deployment.
+# Copyright © Huawei Technologies CO., Ltd. 2020-2020. All rights reserved
+set -e
+
+scope="basic"
+
+if [ $scope == "basic" ]
+then
+  ansible-playbook -vv set_global_env.yaml --tags=basic_only
+  ansible-playbook -vv online_load_images.yaml --tags=basic_only
+  ansible-playbook -vv clean_services.yaml
+  ansible-playbook -vv online_deploy_service.yaml
+elif [ $scope == "additional" ]
+then
+  ansible-playbook -vv set_global_env.yaml
+  ansible-playbook -vv online_install_package.yaml
+  ansible-playbook -vv online_load_images.yaml
+  ansible-playbook -vv init_kubernetes.yaml
+  ansible-playbook -vv clean_services.yaml
+  ansible-playbook -vv online_deploy_service.yaml
+else
+  echo "Wrong deploy scope variable defined."
+fi
