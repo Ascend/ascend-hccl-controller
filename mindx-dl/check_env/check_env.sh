f41265f548b4eebd913add8d691ce0151db2e09b
BianTanggui <kangfuan1@huawei.com>
2020-11-26 10:31:27 +0800

    Match-id-8792caec12599861c64ce1ff06a4b7c36954e9bb

--- mindx-dl/check_env/check_env.sh
+++ mindx-dl/check_env/check_env.sh
@@ -1,7 +1,8 @@
 # Copyright © Huawei Technologies Co., Ltd. 2020. All rights reserved.
 #!/usr/bin/env bash
 
-# 本地检测输出的文件
+# 全局参数
+# 输出文件
 file_dir=$(dirname $(readlink -f $0))
 file_path=${file_dir}"/env_check_report.txt"
 
@@ -10,35 +11,38 @@ SERVICE_CHECK_STR_ARRAY=()
 DEVICE_PLUGIN_SERVICE_NAME="Ascend-Device-Plugin"
 CADVISOR_SERVICE_NAME="cAdvisor"
 HCCL_SERVICE_NAME="HCCL-Controller"
-VOLCANO_SERVICE_NAME="Volcano"
+VOLCANO_SCHEDULER_SERVICE="volcano-scheduler"
+VOLCANO_CONTROLLERS_SERVICE="volcano-controllers"
+VOLCANO_ADMISSION_SERVICE="volcano-admission"
+VOLCANO_ADMISSION_INIT_SERVICE="volcano-admission-init"
 
 # 格式化输出参数,列宽
 service_col_length=0
 status_col_length=0
 version_col_length=0
-# "ip"字符串长度
-default_ip_col_length=2
+default_ip_col_width=2
 
-# 信息
+# 信息常量
 STATUS_NORMAL="normal"
 STATUS_ERROR="error"
 SERVER_NOT_INSTALL="not install"
 POD_STATUS_RUNNING="Running"
+POD_STATUS_COMPLETED="Completed"
 SERVICE_STATUS_RUNNING="active (running)"
 NO_IMAGE="no image"
 
 # 操作命令
-GET_ALL_PODS_COMMAND="unset http_proxy https_proxy && kubectl get pods -A -o wide"
-DESCRIBE_POD_COMMAND="unset http_proxy https_proxy && kubectl describe pod"
+GET_ALL_PODS_COMMAND="kubectl get pods -A -o wide"
+DESCRIBE_POD_COMMAND="kubectl describe pod"
 NPU_INFO_COMMAND="npu-smi info"
 DOCKER_IMAGES="docker images"
 DOKCER_INFO="docker info"
 
-# 检查的节点类型，默认为检查worker
 MASTER_NODE="master"
 WORKER_NODE="worker"
 # 既是master又是worker
 MASTER_WORKER_NODE="master-worker"
+# 检查的节点类型，默认为检查worker
 if [[ $1 == "${MASTER_NODE}" ]]
 then
     check_node_type=${MASTER_NODE}
@@ -48,8 +52,7 @@ then
 else
     check_node_type=${WORKER_NODE}
 fi
-
-# 是否传入ip参数
+# 是否是使用ansible的方式
 if [[ $2 != "" ]]
 then
     ip_addr=$2
@@ -57,7 +60,6 @@ else
     ip_addr=""
 fi
 
-# service,status,version列最大字符串长度
 function set_max_len() {
     service_name_len=`echo "$1" | awk -F '|' '{print length($1)}'`
     status_len=`echo "$1" | awk -F '|' '{print length($2)}'`
@@ -140,9 +142,9 @@ function check_docker() {
     SERVICE_CHECK_STR_ARRAY+=("${check_result_str}")
 }
 
-
-# 检测kubelet状态，版本
+# 检查K8s状态和版本
 function check_kubelet_service() {
+    # ========================================检测kubelet状态，版本 =======================
     service_name="kubelet"
     kubelet_service_status=${SERVER_NOT_INSTALL}
     kubelet_version=""
@@ -159,19 +161,19 @@ function check_kubelet_service() {
     then
         if [[ "${kubelet_status_str}" == "${SERVICE_STATUS_RUNNING}" ]]
         then
-            # kubelet服务正常
+            # kubeletr服务正常
             status=${STATUS_NORMAL}
         else
-            # kubelet服务异常
+            # kubeletr服务异常
             status=${STATUS_ERROR}
         fi
         kubelet_service_status="${status}[$kubelet_status_str]"
 
         kubelet_version=`${kubelet_version_command} 2>/dev/null | awk -F '[" ",]' '{print $2}'`
-        # 找不到kubelet命令
+        # 找不到kubeletr命令
         if [[ "${kubelet_version}" = "" ]]
         then
-            kubelet_version="${STATUS_ERROR}[The kubelet command cannot be found. The kubernetes environment may be damaged.]"
+            kubelet_version="${STATUS_ERROR}[The kubelet command cannot be found. The kubelet environment may be damaged.]"
         fi
     fi
 
@@ -180,8 +182,8 @@ function check_kubelet_service() {
     SERVICE_CHECK_STR_ARRAY+=("${check_result_str}")
 }
 
-# 检测kubeadm状态，版本
 function check_kubeadm_service() {
+    # ========================================检测kubeadm状态，版本 =======================
     service_name="kubeadm"
     kubeadm_service_status=${SERVER_NOT_INSTALL}
     kubeadm_version=""
@@ -189,7 +191,7 @@ function check_kubeadm_service() {
 
     kubeadm_version=`${kubeadm_version_command} 2>/dev/null | awk -F '[,:]' '{print $7}' \
                                                             | sed -e 's/"//g'`
-    # 存在到kubeadmr命令
+    # 找不到kubeadmr命令
     if [[ "${kubeadm_version}" != "" ]]
     then
         kubeadm_service_status=${STATUS_NORMAL}
@@ -201,8 +203,8 @@ function check_kubeadm_service() {
     SERVICE_CHECK_STR_ARRAY+=("${check_result_str}")
 }
 
-# 检测kubectl状态，版本
 function check_kubectl_service() {
+    # ========================================检测kubectl状态，版本 =======================
     service_name="kubectl"
     kubectl_service_status=${SERVER_NOT_INSTALL}
     kubectl_version=""
@@ -211,7 +213,7 @@ function check_kubectl_service() {
     kubectl_version=`${kubectl_version_command} 2>/dev/null | head -n 1 \
                                                             | awk -F '[,:]' '{print $7}' \
                                                             | sed -e 's/"//g'`
-    # 存在kubectl命令
+    # 找不到kubectlr命令
     if [[ "${kubectl_version}" != "" ]]
     then
         kubectl_service_status=${STATUS_NORMAL}
@@ -223,7 +225,6 @@ function check_kubectl_service() {
     SERVICE_CHECK_STR_ARRAY+=("${check_result_str}")
 }
 
-# 检查K8s状态和版本
 function check_k8s() {
     check_kubelet_service
     check_kubeadm_service
@@ -247,6 +248,7 @@ function check_mindxdl_by_name() {
     # 节点部署了服务
     if [[ "${service_status_str}" != "" ]]
     then
+
         # 使用的k8s命名空间
         pod_namespace=`echo "${service_status_str}" | awk -F ' ' '{print $1}'`
         # pod名称
@@ -260,15 +262,25 @@ function check_mindxdl_by_name() {
                                                            | awk -F ' ' '{print $2}' \
                                                            | sed -e 's/[ ]*$//g' \
                                                            | sed -e 's/^[ ]*//g'`
-        if [[ "${pod_status}" != "${POD_STATUS_RUNNING}" ]]
+        if [[ ${service_name} == ${VOLCANO_ADMISSION_INIT_SERVICE} ]]
         then
-            status=${STATUS_ERROR}
+            if [[ "${pod_status}" != "${POD_STATUS_COMPLETED}" ]]
+            then
+                status=${STATUS_ERROR}
+            else
+                status=${STATUS_NORMAL}
+            fi
         else
-            status=${STATUS_NORMAL}
+            if [[ "${pod_status}" != "${POD_STATUS_RUNNING}" ]]
+            then
+                status=${STATUS_ERROR}
+            else
+                status=${STATUS_NORMAL}
+            fi
         fi
         service_status="${status}[$pod_status]"
     else
-        service_image=`${DOCKER_IMAGES} 2>/dev/null | grep -E "${image_name}" \
+        service_image=`${DOCKER_IMAGES} 2>/dev/null | grep -E "${image_name} " \
                                                     | awk -F ' ' '{print $1":"$2}' \
                                                     | xargs \
                                                     | sed -e 's/ /,/g'`
@@ -321,6 +333,9 @@ function check_hccl_service() {
 
 # 检查volcano-admission状态，版本
 function check_volcano_admission_service() {
+    # volcano_admission
+    service_name=${VOLCANO_ADMISSION_SERVICE}
+
     # pod名称通配符
     service_name_regexp="volcano-admission-(.*)"
 
@@ -328,11 +343,14 @@ function check_volcano_admission_service() {
     image_name="volcanosh/vc-webhook-manager"
 
     # 检查
-    check_mindxdl_by_name ${service_name_regexp} ${image_name} ${VOLCANO_SERVICE_NAME}
+    check_mindxdl_by_name ${service_name_regexp} ${image_name} ${service_name}
 }
 
 # 检查volcano-controllers-状态，版本
 function check_volcano_controllers_service() {
+    # volcano-controllers
+    service_name=${VOLCANO_CONTROLLERS_SERVICE}
+
     # pod名称通配符
     service_name_regexp="volcano-controllers-(.*)"
 
@@ -340,11 +358,14 @@ function check_volcano_controllers_service() {
     image_name="volcanosh/vc-controller-manager"
 
     # 检查
-    check_mindxdl_by_name ${service_name_regexp} ${image_name} ${VOLCANO_SERVICE_NAME}
+    check_mindxdl_by_name ${service_name_regexp} ${image_name} ${service_name}
 }
 
 # 检查volcano-scheduler状态，版本
 function check_volcano_scheduler_service() {
+    # volcano-controllers
+    service_name=${VOLCANO_SCHEDULER_SERVICE}
+
     # pod名称通配符
     service_name_regexp="volcano-scheduler-(.*)"
 
@@ -352,28 +373,22 @@ function check_volcano_scheduler_service() {
     image_name="volcanosh/vc-scheduler"
 
     # 检查
-    check_mindxdl_by_name ${service_name_regexp} ${image_name} ${VOLCANO_SERVICE_NAME}
+    check_mindxdl_by_name ${service_name_regexp} ${image_name} ${service_name}
 }
 
-# 检查volcano-base状态，版本
-function check_volcano_base_service() {
-    image_name="volcanosh/vc-webhook-manager-base"
-    service_image=`${DOCKER_IMAGES} 2>/dev/null | grep -E "${image_name}" \
-                                                | awk -F ' ' '{print $1":"$2}' \
-                                                | xargs \
-                                                | sed -e 's/ /,/g'`
+# 检查volcano-admission-init状态，版本
+function check_volcano_admission_init_service() {
+    # volcano-admission-init
+    service_name=${VOLCANO_ADMISSION_INIT_SERVICE}
 
-    if [[ "${service_image}" != "" ]]
-    then
-        service_status=${STATUS_NORMAL}
-    else
-        service_status=${SERVER_NOT_INSTALL}
-        service_image=${NO_IMAGE}
-    fi
+    # pod名称通配符
+    service_name_regexp="volcano-admission-init-(.*)"
 
-    check_result_str="${VOLCANO_SERVICE_NAME}|${service_status}|${service_image}"
-    set_max_len "${check_result_str}"
-    SERVICE_CHECK_STR_ARRAY+=("${check_result_str}")
+    # volcano_admission服务使用镜像的名称
+    image_name="volcanosh/vc-webhook-manager"
+
+    # 检查
+    check_mindxdl_by_name ${service_name_regexp} ${image_name} ${service_name}
 }
 
 # 检查MindX DL组件状态和版本
@@ -383,7 +398,7 @@ function check_mindxdl() {
     then
         check_hccl_service
         check_volcano_admission_service
-        check_volcano_base_service
+        check_volcano_admission_init_service
         check_volcano_controllers_service
         check_volcano_scheduler_service
     fi
@@ -417,26 +432,22 @@ function print_format_to_file() {
         hostname_col_length=${table_head_hostname_col_length}
     fi
 
-    # ip地址长度
     ip_addr_length=$(echo "${ip_addr}" | awk '{print length($0)}')
-    # ip列字符串长度
-    ip_col_length=${default_ip_col_length}
-    if (( ${ip_addr_length} > ${default_ip_col_length} ))
+    ip_col_length=${default_ip_col_width}
+    if (( ${ip_addr_length} > ${default_ip_col_width} ))
     then
         ip_col_length=${ip_addr_length}
     fi
 
-    # 一行字符串长度
-    row_str_length=$((${service_col_length} + ${ip_col_length} + \
+    row_max_length=$((${service_col_length} + ${ip_col_length} + \
                         ${status_col_length} + ${version_col_length} + \
                         ${hostname_col_length} + ${other_symbel_length}))
 
-    printf "%-${row_str_length}s\n" "-" | sed -e 's/ /-/g' >> ${file_path}
+    printf "%-${row_max_length}s\n" "-" | sed -e 's/ /-/g' >> ${file_path}
     for((i=0; i<${arr_length}; i++));
     do
         if (( $i == 0 ))
         then
-            # 表头
             table_hostname=`echo "${SERVICE_CHECK_STR_ARRAY[i]}" | awk -F '|' '{print $1}'`
             table_ip_addr=`echo "${SERVICE_CHECK_STR_ARRAY[i]}" | awk -F '|' '{print $2}'`
             table_service_name=`echo "${SERVICE_CHECK_STR_ARRAY[i]}" | awk -F '|' '{print $3}'`
@@ -452,7 +463,7 @@ function print_format_to_file() {
 
         printf "| %-${hostname_col_length}s | %-${ip_col_length}s | %-${service_col_length}s | %-${status_col_length}s | %-${version_col_length}s |\n" \
                 "${table_hostname}" "${table_ip_addr}" "${table_service_name}" "${table_status}" "${table_version}" >> ${file_path}
-        printf "%-${row_str_length}s\n" "-" | sed -e 's/ /-/g' >> ${file_path}
+        printf "%-${row_max_length}s\n" "-" | sed -e 's/ /-/g' >> ${file_path}
     done
 }
 
@@ -494,5 +505,5 @@ function main() {
 main >>/dev/null 2>&1
 cat ${file_path}
 echo ""
-echo "Finished! The check report is stored in the ${file_path}"
+echo "Notice: Finished! The check report is stored in the ${file_path}"
 echo ""
