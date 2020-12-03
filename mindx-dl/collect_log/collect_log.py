661fa7eac36a1a106b27d62b90451920442db476
BianTanggui <biantanggui@huawei.com>
2020-12-03 17:21:53 +0800

    Match-id-6141981158aba19fb94e10b936da68f15bec9ccf

--- mindx-dl/collect_log/collect_log.py
+++ mindx-dl/collect_log/collect_log.py
@@ -15,7 +15,7 @@
 #  limitations under the License.
 
 import gzip
-import os
+import os, stat
 import platform
 import socket
 import tarfile
@@ -36,16 +36,14 @@ def get_create_time():
 def compress(src, dst):
     """compress if the src file is uncompressed"""
     log("compress files:" + src)
-    if src[-3:].lower() == ".gz":
+    if src.lower().endswith(".gz"):
         f_open = open
     else:
         f_open = gzip.open
         dst = dst + ".gz"
-    f_open_dst = f_open(dst, 'wb', 9)
-    f_open_src = open(src, 'rb')
-    f_open_dst.writelines(f_open_src)
-    f_open_dst.close()
-    f_open_src.close()
+    with f_open(dst, 'wb', 9) as f_open_dst, open(src, 'rb') as f_open_src:
+        f_open_dst.writelines(f_open_src)
+
     return dst
 
 
@@ -54,12 +52,12 @@ def get_compress_file_paths(tmp_path, dst_src_paths, done):
         if not os.path.exists(dst_path):
             os.makedirs(dst_path)
     for dst_path, src_path in dst_src_paths:
-        filenames = []
-        for _, _, filenames in os.walk(src_path):
+        file_names = []
+        for _, _, file_names in os.walk(src_path):
             break
-        for file in filenames:
-            src = os.path.join(src_path, file)
-            dst = os.path.join(dst_path, file)
+        for tmp_file in file_names:
+            src = os.path.join(src_path, tmp_file)
+            dst = os.path.join(dst_path, tmp_file)
             log("Compressing: %s\n" % dst)
             try:
                 dst = compress(src, dst)
@@ -79,33 +77,35 @@ def compress_os_files(base, tmp_path, done):
     elif "debian" in sys_str:
         os_log_file = "syslog"
     else:
-        os_log_file = ""
+        log("not support os inf %s\n" % sys_str)
+        return done
 
     log_path = []
     os_log_path = "/var/log/"
     files = os.listdir(os_log_path)
-    for file in files:
-        if os_log_file in file:
-            log_file_path = os_log_path + file
+    for tmp_file in files:
+        if os_log_file in tmp_file:
+            log_file_path = os_log_path + tmp_file
             log_path.append(log_file_path)
 
-    for dst_path, file_list in [(base + "/OS_log", log_path)]:
-        os.mkdir(dst_path)
-        for file in file_list:
-            if os.path.isfile(file):
-                dst = os.path.join(dst_path, os.path.split(file)[1])
-                dst = compress(file, dst)
-                done.append(dst[len(tmp_path) + 1:])
+    dst_path = base + "/OS_log"
+    os.mkdir(dst_path)
+    for tmp_file in log_path:
+        if os.path.isfile(tmp_file):
+            dst = os.path.join(dst_path, os.path.split(tmp_file)[1])
+            dst = compress(tmp_file, dst)
+            relative_path = dst[len(tmp_path) + 1:]
+            done.append(relative_path)
     return done
 
 
 def compress_file_list(base, tmp_path, dst_src_file_list, done):
     for dst_path, file_list in dst_src_file_list:
-        for file in file_list:
-            if os.path.isfile(file):
-                dst = os.path.join(dst_path, os.path.split(file)[1])
+        for tmp_file in file_list:
+            if os.path.isfile(tmp_file):
+                dst = os.path.join(dst_path, os.path.split(tmp_file)[1])
                 log("Compressing: %s\n" % dst)
-                dst = compress(file, dst)
+                dst = compress(tmp_file, dst)
                 done.append(dst[len(tmp_path) + 1:])
 
     done = compress_os_files(base, tmp_path, done)
@@ -123,14 +123,14 @@ def set_log_report_file_path():
     host_name = socket.gethostname()
     tmp_path = os.path.join(os.getcwd(), time_base)
     base = os.path.join(tmp_path, "LogCollect")
-    tar_file_ath = tmp_path + "-" + host_name + "-LogCollect.gz"
+    tar_file_path = tmp_path + "-" + host_name + "-LogCollect.gz"
 
     # create folders
     log("Creating dst folder:" + base)
     os.makedirs(tmp_path)
     os.makedirs(base)
 
-    return base, tmp_path, tar_file_ath
+    return base, tmp_path, tar_file_path
 
 
 def get_log_path_src_and_dst(base):
@@ -162,11 +162,11 @@ def create_compress_file(done, tmp_path, tar_file_path):
     # create a tar file, and archive all compressed files into ita
     log("create tar file:" + tar_file_path + ", from all compressed files")
     try:
-        with tarfile.open(tar_file_path, 'w:gz') as file:
+        with tarfile.open(tar_file_path, 'w:gz') as tmp_file:
             old_path = os.getcwd()
             os.chdir(tmp_path)
             for filename in done:
-                file.add(filename)
+                tmp_file.add(filename)
                 log("Adding to tar: %s\n" % filename)
             os.chdir(old_path)
     except tarfile.TarError as err:
@@ -174,8 +174,7 @@ def create_compress_file(done, tmp_path, tar_file_path):
 
 
 def set_file_right(tar_file_path):
-    uid = getpwnam("hwMindX").pw_uid
-    os.lchown(tar_file_path, uid, uid)
+    os.chmod(tar_file_path, stat.S_IREAD)
 
 
 def delete_tmp_file(tmp_path):
