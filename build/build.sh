38fb907dd2f86ca0f0a84c0b73e79f57f3e9a928
BianTanggui <tutianyi@huawei.com>
2021-10-18 17:20:25 +0800

    Match-id-2135d1694c93f0721f27e33779566f588318d563

--- build/build.sh
+++ build/build.sh
@@ -3,13 +3,13 @@
 # Copyright @ Huawei Technologies CO., Ltd. 2020-2020. All rights reserved
 
 set -e
-CUR_DIR=$(dirname "$(readlink -f "$0")")
-TOP_DIR=$(realpath "${CUR_DIR}"/..)
+cur_dir=$(dirname "$(readlink -f "$0")")
+top_dir=$(realpath "${cur_dir}"/..)
 export GO111MODULE="on"
-VER_FILE="${TOP_DIR}"/service_config.ini
+ver_file="${top_dir}"/service_config.ini
 build_version="beta"
-if [ -f "$VER_FILE" ]; then
-  line=$(sed -n '3p' "$VER_FILE" 2>&1)
+if [ -f "$ver_file" ]; then
+  line=$(sed -n '3p' "$ver_file" 2>&1)
   #cut the chars after ':'
   build_version=${line#*:}
 fi
@@ -17,27 +17,26 @@ fi
 arch=$(arch 2>&1)
 echo "Build Architecture is" "${arch}"
 
+sed -i "s/hccl-controller:.*/hccl-controller:${build_version}/" "${top_dir}"/build/hccl-controller.yaml
 
-sed -i "s/hccl-controller:.*/hccl-controller:${build_version}/" "${TOP_DIR}"/build/hccl-controller.yaml
+output_name="hccl-controller"
 
-OUTPUT_NAME="hccl-controller"
-
-DOCKER_FILE_NAME="Dockerfile"
+docker_file_name="Dockerfile"
 docker_zip_name="hccl-controller-${build_version}-${arch}.tar.gz"
 docker_images_name="hccl-controller:${build_version}"
-function clear_env() {
-  rm -rf "${TOP_DIR}"/output/*
+function clean() {
+  rm -rf "${top_dir}"/output/*
 }
 
 function build() {
-  cd "${TOP_DIR}"
+  cd "${top_dir}"
 
   CGO_CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv"
   CGO_CPPFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv"
-  CGO_ENABLED=0 go build -mod=mod -buildmode=pie   -ldflags "-s -linkmode=external -extldflags=-Wl,-z,now  -X main.BuildName=${OUTPUT_NAME} \
+  CGO_ENABLED=0 go build -mod=mod -buildmode=pie -ldflags "-s -linkmode=external -extldflags=-Wl,-z,now  -X main.BuildName=${output_name} \
             -X main.BuildVersion=${build_version}_linux-${arch}" \
-            -o ${OUTPUT_NAME}
-  ls ${OUTPUT_NAME}
+  -o ${output_name}
+  ls ${output_name}
   if [ $? -ne 0 ]; then
     echo "fail to find hccl-controller"
     exit 1
@@ -45,23 +44,26 @@ function build() {
 }
 
 function mv_file() {
-  mv "${TOP_DIR}"/${OUTPUT_NAME} "${TOP_DIR}"/output
-  cp "${TOP_DIR}"/build/hccl-controller.yaml "${TOP_DIR}"/output/hccl-controller-"${build_version}".yaml
-  cp "${TOP_DIR}"/build/${DOCKER_FILE_NAME} "${TOP_DIR}"/output
+  mv "${top_dir}"/${output_name} "${top_dir}"/output
+  cp "${top_dir}"/build/hccl-controller.yaml "${top_dir}"/output/hccl-controller-"${build_version}".yaml
+  cp "${top_dir}"/build/${docker_file_name} "${top_dir}"/output
 }
 
 function change_mod() {
-  chmod 400 "${TOP_DIR}"/output/*
-  chmod 500 "${TOP_DIR}/output/${OUTPUT_NAME}"
+  chmod 400 "${top_dir}"/output/*
+  chmod 500 "${top_dir}/output/${output_name}"
 }
 
-
-
 function main() {
-  clear_env
+  clean
   build
   mv_file
   change_mod
 }
+
+if [ "$1" = clean ]; then
+  clean
+  exit 0
+fi
 
 main
